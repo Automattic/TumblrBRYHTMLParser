8142fa9840e0ee84710dcf871753cf04edb3c8e5
Daniel Bernal <yo@danielbernal.co>
2021-02-18 12:18:05 -0500

    Added Support for SwiftPackage Manager

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BRYHTMLParser",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "BRYHTMLParser",
+            targets: ["BRYHTMLParser"]),
+    ],
+    targets: [
+        .target(
+            name: "BRYHTMLParser",
+            dependencies: [],
+            path: "BRYHTMLParser",
+            publicHeadersPath: "include")
+    ]
+)
